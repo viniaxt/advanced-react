0797d1b6332459b80a26885f4128c42fc782c7d9
Vinicius Axt <viniciusaxt@gmail.com>
2021-03-14 12:19:47 -0300

    Husky and lint-staged configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
 
-const xd = 'd'
-
 export default function Home({ title = '' }) {
   return (
     <div className={styles.container}>
