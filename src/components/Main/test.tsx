52d3623ccb4a1a61919bdf8e0173dd3fb745baf1
Vinicius Axt <viniciusaxt@gmail.com>
2021-08-29 12:45:08 -0300

    Created first component with styles

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -14,4 +14,12 @@ describe('<Main />', () => {
 
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('Should render colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b'
+    })
+  })
 })
